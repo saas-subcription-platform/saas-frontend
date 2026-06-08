3ad8489f4879100a4e092c517344209f29209327
Needhi Patil <pneedhi03@gmail.com>
2026-06-08 16:14:57 +0530

    Implemented Add/Edit User Functionality and Navigation Improvements - Added Add User Page - Added Edit User Page - Added Back navigation to User Details, Add User and Company Profile pages - Improved Notifications Page layout and spacing

--- frontend/src/app/routes/AppRoutes.jsx
+++ frontend/src/app/routes/AppRoutes.jsx
@@ -1,24 +1,32 @@
-import { Routes, Route } from "react-router-dom";
-import LandingPage from "../../features/landing/pages/landingPage";
-import DashboardPage from "../../features/Admin/pages/DashboardPage";
-import UsersPage from "../../features/Admin/pages/UsersPage";
-import UserDetailsPage from "../../features/Admin/pages/UserDetailsPage";
-import NotificationsPage from "../../features/Admin/pages/NotificationsPage";
-import CompanyProfile from "../../features/Admin/pages/CompanyProfilePage";
+    import { Routes, Route } from "react-router-dom";
+    import LandingPage from "../../features/landing/pages/landingPage";
+    import DashboardPage from "../../features/Admin/pages/DashboardPage";
+    import UsersPage from "../../features/Admin/pages/UsersPage";
+    import UserDetailsPage from "../../features/Admin/pages/UserDetailsPage";
+    import NotificationsPage from "../../features/Admin/pages/NotificationsPage";
+    import CompanyProfilePage from "../../features/Admin/pages/CompanyProfilePage";
+    import EditUserPage from "../../features/Admin/pages/EditUserPage";
+    import AddUserPage from "../../features/Admin/pages/AddUserPage";
+    import EditCompanyPage from "../../features/Admin/pages/EditCompanyPage";
 
-function AppRoutes(){
-    return (
-        <Routes>
-            <Route path="/" element={<LandingPage/>}/>
-            <Route path="/admin/dashboard" element={<DashboardPage/>}/>
-            <Route path="/admin/users" element={<UsersPage />} />
-            <Route path="/admin/users/:id" element={<UserDetailsPage/>} />
-            <Route path="/admin/notifications" element={<NotificationsPage/>} />
-            <Route path="/admin/company" element={<CompanyProfile/>} />
 
-        </Routes>
-    )
-}
+    function AppRoutes(){
+        return (
+            <Routes>
+                <Route path="/" element={<LandingPage/>}/>
+                <Route path="/admin/dashboard" element={<DashboardPage/>}/>
+                <Route path="/admin/users" element={<UsersPage />} />
+                <Route path="/admin/users/add" element={<AddUserPage/>} />
+                <Route path="/admin/users/:id" element={<UserDetailsPage/>} />
+                <Route path="/admin/notifications" element={<NotificationsPage/>} />
+                <Route path="/admin/company" element={<CompanyProfilePage/>} />
+                <Route path="/admin/company/edit" element={<EditCompanyPage/>} />
+                <Route path="/admin/users/edit/:id" element={<EditUserPage/>} />
 
-export default AppRoutes;
+
+            </Routes>
+        )
+    }
+
+    export default AppRoutes;
 
