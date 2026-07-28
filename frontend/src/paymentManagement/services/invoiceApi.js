a73294df7705767473b0f66729daa1b6e3dad008
Needhi Patil <pneedhi03@gmail.com>
2026-07-28 19:33:52 +0530

    feat: add frontend APIs for payment management
    - Add payment API service
    - Add transaction API service
    - Add invoice API service

---
+++ frontend/src/paymentManagement/services/invoiceApi.js
@@ -0,0 +1,22 @@
+import api from "../../api/api";
+
+export const getAllInvoices = async () => {
+    const response = await api.get("/invoices");
+    return response.data;
+};
+
+export const getInvoiceById = async (id) => {
+    const response = await api.get(`/invoices/${id}`);
+    return response.data;
+};
+
+export const downloadInvoice = async (id) => {
+    const response = await api.get(
+        `/invoices/${id}/download`,
+        {
+            responseType: "blob"
+        }
+    );
+
+    return response.data;
+};
