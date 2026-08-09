c06effc8f77d41b6ff6b4cd321af15665210f0ab
Shruti More <moreshruti382@gmail.com>
2026-08-09 15:23:14 +0530

    Built UI for chatbot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import AppRoutes from './app/routes/AppRoutes'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 import ScrollToTop from './components/ui/ScrollToTop'
 
-
 function App() {
-
   return (
     <>
       <AppRoutes/>
