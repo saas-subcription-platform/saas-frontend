ce972d5bbbb0bdc4377fcc9dcc1f86c6785ce118
ShreyaKaranjikar <nshreya931@gmail.com>
2026-08-09 18:10:14 +0530

    -changed the urls for both production and local enviroment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:9090",
+    baseURL: import.meta.env.VITE_SPRING_API,
     headers: {
     "Content-Type": "application/json",
   },
